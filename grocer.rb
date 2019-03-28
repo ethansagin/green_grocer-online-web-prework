2f49580e5638c4f7b4f07ecec29fcf2ce6df3b6c
Ethan <ethansagin@gmail.com>
2019-03-28 13:40:28 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,32 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+  consolidated = {}
+  cart.each { |item|
+    item.each { |fruit, hsh|
+      consolidated[fruit] ||= hsh
+      consolidated[fruit][:count] ||= 0
+      consolidated[fruit][:count] += 1
+    }
+  }
+  consolidated
 end
 
+
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each { |coup_hsh|
+    fruit_name = coup_hsh[:item]
+    new_coup_hsh = {
+      :price => coup_hsh[:cost],
+      :clearance => true,
+      :count => 0}
+      binding.pry
+    if cart.key? == fruit_name
+      cart[fruit_name][:clearance] = new_coup_hsh[:clearance]
+    if 
+      binding.pry
+    end
+  }
 end
 
 def apply_clearance(cart)
