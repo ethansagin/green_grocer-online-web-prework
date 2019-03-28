6bb11797b63fb695c36c13a4b68e74529327b5f0
Ethan <ethansagin@gmail.com>
2019-03-28 14:09:04 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -25,8 +25,8 @@ def apply_coupons(cart, coupons)
     if cart.key?(fruit_name)
       cart[fruit_name][:clearance] = new_coup_hsh[:clearance]
       if cart[fruit_name][:count] >= coup_hsh[:num]
-        cart[fruit_name][:count] = cart[fruit_name][:count] % coup_hsh[:num]
         new_coup_hsh[:count] = cart[fruit_name][:count] / coup_hsh[:num]
+        cart[fruit_name][:count] = cart[fruit_name][:count] % coup_hsh[:num]
       end
       new_fruit_name = fruit_name + "/W COUPONS"
       cart[new_fruit_name] = new_coup_hsh
